fe9768af447689a96e55ad35c82f42ebb4d61ade
dmitry kolesov <dmitry.kolesov@gmail.com>
2013-11-13 22:48:19 +0700

    first commit citites test

---
+++ Cities12/src/main/java/com/azcltd/android/test/kolesov/City.java
@@ -0,0 +1,43 @@
+package com.azcltd.android.test.kolesov;
+
+import android.location.Location;
+
+/**
+ * Created by 123 on 13.11.13.
+ */
+public class City {
+
+    private int id;
+    private String name;
+    private String description;
+    private String image_url;
+    private String country;
+    private Location location;
+
+    public int GetId()
+    { return id;}
+    public String GetName()
+    { return name;}
+    public String GetImageUrl()
+    { return image_url;}
+    public String GetCountry()
+    { return country;}
+    public Location GetLocation()
+    { return location;}
+
+    public City(int id,
+            String name,
+            String description,
+            String image_url,
+            String country,
+            Location location)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.image_url = image_url;
+        this.country = country;
+        this.location = location;
+    }
+
+}
---
+++ Cities12/src/main/java/com/azcltd/android/test/kolesov/WsClient.java
@@ -0,0 +1,150 @@
+package com.azcltd.android.test.kolesov;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 123 on 13.11.13.
+ */
+
+public class WsClient {
+
+    static final String wsUrl = "http://ec2-54-235-247-20.compute-1.amazonaws.com/android/cities.json";
+    private List<City> cities;
+
+    public List<City> GetCitites()
+    {
+        // get from http://stackoverflow.com/questions/7281283/android-webrequest-simple-solution
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet(wsUrl);
+
+        // Execute the request
+        HttpResponse response;
+        try {
+            response = client.execute(httpget);
+
+            // Get hold of the response entity
+            HttpEntity entity = response.getEntity();
+            // If the response does not enclose an entity, there is no need
+            // to worry about connection release
+
+            if (entity != null) {
+
+                // A Simple JSON Response Read
+                InputStream instream = entity.getContent();
+                String result = convertStreamToString(instream);
+                // now you have the string representation of the HTML request
+                instream.close();
+
+                cities = ConvertJsonStringToCities(result);
+            }
+
+
+        } catch (Exception e) {
+            ;
+        }
+        return cities;
+    }
+
+    private static String convertStreamToString(InputStream is) {
+    /*
+     * To convert the InputStream to String we use the BufferedReader.readLine()
+     * method. We iterate until the BufferedReader return null which means
+     * there's no more data to read. Each line will appended to a StringBuilder
+     * and returned as String.
+     */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+    private List<City> ConvertJsonStringToCities(String Content)
+    {
+        JSONObject jsonResponse;
+        List<City> cities  = new ArrayList<City>();
+
+        //http://androidexample.com/Restful_Webservice_Call_And_Get_And_Parse_JSON_Data-_Android_Example/index.php?view=article_discription&aid=101&aaid=123
+        try {
+
+            /****** Creates a new JSONObject with name/value mappings from the JSON string. ********/
+            jsonResponse = new JSONObject(Content);
+
+            /***** Returns the value mapped by name if it exists and is a JSONArray. ***/
+            /*******  Returns null otherwise.  *******/
+            JSONArray jsonMainNode = jsonResponse.optJSONArray("Android");
+
+            /*********** Process each JSON Node ************/
+
+            int lengthJsonArr = jsonMainNode.length();
+
+            for(int i=0; i < lengthJsonArr; i++)
+            {
+                /****** Get Object for each JSON node.***********/
+                JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+
+                /******* Fetch node values **********/
+                int id       = jsonChildNode.optInt("id");
+                String name     = jsonChildNode.optString("name").toString();
+                String description = jsonChildNode.optString("description").toString();
+                String image_url = jsonChildNode.optString("image_url").toString();
+                String country = jsonChildNode.optString("country").toString();
+
+                JSONObject jsonResponse1 = new JSONObject(jsonChildNode.optString("location").toString());
+                double latitude = jsonResponse1.optDouble("latitude");
+                double longitude = jsonResponse1.optDouble("longitude");
+
+                Location location = new Location("myProvider");
+                location.setLongitude(longitude);
+                location.setLatitude(latitude);
+
+                cities.add(new City(id, description, name, image_url, country, location));
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return cities;
+    }
+}
